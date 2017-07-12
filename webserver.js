d091dc88cdd80176116f54f2704402689c7d0b1d
Michael Bykovski <mbykovski@seibert-media.net>
2017-07-12 10:44:44 +0200

    Write Webserver in ES6

--- webserver.js
+++ webserver.js
@@ -1,50 +1,43 @@
-var WEBSERVER_CLASS = "Webserver";
+'use strict';
 
-exports.start = function(success_callback, error_callback, port) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.start = start;
+exports.onRequest = onRequest;
+exports.sendResponse = sendResponse;
+exports.stop = stop;
+
+var _exec = require('cordova/exec');
+
+var _exec2 = _interopRequireDefault(_exec);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var WEBSERVER_CLASS = 'Webserver';
+var START_FUNCTION = 'start';
+var ONREQUEST_FUNCTION = 'onRequest';
+var SENDRESPONSE_FUNCION = 'sendResponse';
+var STOP_FUNCTION = 'stop';
+
+function start(success_callback, error_callback, port) {
   var params = [];
   if (port) {
     params.push(port);
   }
-  cordova.exec(
-    success_callback,
-    error_callback,
-    WEBSERVER_CLASS,
-    "start",
-    params
-  );
-};
+  (0, _exec2.default)(success_callback, error_callback, WEBSERVER_CLASS, START_FUNCTION, params);
+}
 
-exports.onRequest = function(success_callback) {
-  cordova.exec(
-    success_callback,
-    function(error) {console.error(error)},
-    WEBSERVER_CLASS,
-    "onRequest",
-    []
-  );
-};
+function onRequest(success_callback) {
+  (0, _exec2.default)(success_callback, function (error) {
+    console.error(error);
+  }, WEBSERVER_CLASS, ONREQUEST_FUNCTION, []);
+}
 
-exports.sendResponse = function(
-  requestId,
-  params,
-  success_callback,
-  error_callback
-) {
-  cordova.exec(
-    success_callback,
-    error_callback,
-    WEBSERVER_CLASS,
-    "sendResponse",
-    [requestId, params]
-  );
-};
+function sendResponse(requestId, params, success_callback, error_callback) {
+  (0, _exec2.default)(success_callback, error_callback, WEBSERVER_CLASS, SENDRESPONSE_FUNCION, [requestId, params]);
+}
 
-exports.stop = function(success_callback, error_callback) {
-  cordova.exec(
-    success_callback,
-    error_callback,
-    WEBSERVER_CLASS,
-    "stop",
-    []
-  );
-};
+function stop(success_callback, error_callback) {
+  (0, _exec2.default)(success_callback, error_callback, WEBSERVER_CLASS, STOP_FUNCTION, []);
+}
